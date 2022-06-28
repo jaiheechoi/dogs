a5093c20d96187d09fd0592610236e3321946ec2
Jaihee Choi <jc155@rice.edu>
2022-06-28 13:06:25 -0500

    added puppies

--- R/dog_function.R
+++ R/dog_function.R
@@ -9,7 +9,7 @@
 #' 
 dog_function <- function(love=TRUE){
   if(love==TRUE){
-    print("I love dogs!")
+    print("I love dogs and puppies!")
   }
   else {
     print("I am not a cool person!!!!!!!!!!!!")
